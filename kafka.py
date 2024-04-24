6a11a8936c9e2884bf6439129c2f68763fb744dc
Damien SALEM <d.salem@ecole-ipssi.net>
2024-04-24 15:27:08 +0200

    kafka

--- kafka.py
+++ kafka.py
@@ -1,5 +1,7 @@
-from confluent_kafka import Consumer, KafkaError
+import requests
+from confluent_kafka import Consumer, KafkaError, Producer
 from hdfs import InsecureClient
+import json
 
 # Configuration Kafka
 kafka_bootstrap_servers = 'localhost:9092'
@@ -9,32 +11,51 @@ kafka_topic = 'topic1'
 hdfs_namenode = 'http://localhost:9870'
 hdfs_path = '/user/hadoop/data/'
 
+# Configuration du client HDFS
+hdfs_client = InsecureClient(hdfs_namenode, user='hadoop')
+
+# Configuration Kafka producer
+conf_p ={
+    'bootstrap.servers': kafka_bootstrap_servers
+}
+
 # Configuration du client Kafka Consumer
-conf = {
+conf_c = {
     'bootstrap.servers': kafka_bootstrap_servers,
     'group.id': 'my_consumer_group',
     'auto.offset.reset': 'earliest'
 }
-
-consumer = Consumer(conf)
+producer = Producer(conf_p)
+consumer = Consumer(conf_c)
 consumer.subscribe([kafka_topic])
 
-# Configuration du client HDFS
-hdfs_client = InsecureClient(hdfs_namenode, user='hadoop')
+def produce_message(data):
+    producer.produce(kafka_topic,json.dumps(data).encode('utf-8'))
+    producer.flush()
 
-# Consommation des messages Kafka et écriture dans HDFS
-while True:
-    msg = consumer.poll(timeout=1.0)
-    if msg is None:
-        continue
-    if msg.error():
-        if msg.error().code() == KafkaError._PARTITION_EOF:
+def consume_message():
+
+    # Consommation des messages Kafka et écriture dans HDFS
+    while True:
+        msg = consumer.poll(timeout=1.0)
+        if msg is None:
             continue
-        else:
-            print(msg.error())
-            break
-    hdfs_file_path = hdfs_path + 'data.txt'
-    with hdfs_client.write(hdfs_file_path, overwrite=True) as writer:
-        writer.write(msg.value() + b'\n')
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                continue
+            else:
+                print(msg.error())
+                break
+        hdfs_file_path = hdfs_path + 'data.txt'
+        with hdfs_client.write(hdfs_file_path, overwrite=True) as writer:
+            writer.write(msg.value() + b'\n')
 
-consumer.close()
+def probe_request():
+    container_ip="172.24.0.7"
+    res= requests.get(f"http://{container_ip}:5000/lastscan")
+    produce_message(res)
+
+if __name__ == "__main__":
+    probe_request()
+    producer.close()
+    consumer.close()
--- test_probe.py
+++ test_probe.py
@@ -1,8 +1,5 @@
 import requests
 
-
-
-
 container_ip="localhost"
 res= requests.get(f"http://{container_ip}:5000/lastscan")
 print(res)
